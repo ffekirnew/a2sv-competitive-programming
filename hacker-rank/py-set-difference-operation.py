31f40b84546bcc24e0c3ccc8db21ef2d74678af4
ffekirnew <ffekirnew0808@gmail.com>
2022-12-26 17:35:46 +0300

    added my solution to Set Difference Operation at HackerRank

---
+++ hacker-rank/py-set-difference-operation.py
@@ -0,0 +1,12 @@
+def main():
+    # take in inputs
+    num_english_subscribers = int(input())
+    english_subscribers = set([student for student in input().split()])
+    num_french_subscribers = int(input())
+    french_subscribers = set([student for student in input().split()])
+    # print the solution
+    print(len(english_subscribers.difference(french_subscribers)))
+
+    
+if __name__ == "__main__":
+    main()
