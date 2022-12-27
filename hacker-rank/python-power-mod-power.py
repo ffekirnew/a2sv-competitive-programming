a1baceca5d9b5bd44b2f1fae560a676f5bd7ebd4
ffekirnew <ffekirnew0808@gmail.com>
2022-12-27 19:52:16 +0300

    added my solution to Power mod Power at HackerRank

---
+++ hacker-rank/python-power-mod-power.py
@@ -0,0 +1,10 @@
+def main():
+    # accept the inputs
+    a = int(input())
+    b = int(input())
+    m = int(input())
+    print(a ** b)
+    print(a ** b % m)
+
+if __name__ == "__main__":
+    main()
