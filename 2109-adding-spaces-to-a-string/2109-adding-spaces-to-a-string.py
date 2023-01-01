29659a7a0ae714e069d95c363d1b64354892461c
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-01-01 16:32:03 +0300

    Time: 858 ms (74.91%), Space: 51.6 MB (35.64%) - LeetHub

---
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.py
@@ -0,0 +1,15 @@
+class Solution:
+    def addSpaces(self, s: str, spaces: List[int]) -> str:
+        # create the object to be returned
+        answer = []
+        # iterate over the string and add the spaces
+        space_ctr = 0
+        for str_index, char in enumerate(s):
+            if space_ctr < len(spaces) and str_index == spaces[space_ctr]:
+                answer.append(" ")
+                space_ctr += 1
+            answer.append(char)
+        # return the solution
+        print("".join(answer))
+        return "".join(answer)
+        
