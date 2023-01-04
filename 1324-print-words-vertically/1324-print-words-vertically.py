8effc2cdf155adcf5f8f265e2c010eacbf920abc
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-01-04 18:04:02 +0300

    Time: 60 ms (33.46%), Space: 14 MB (14.97%) - LeetHub

---
+++ 1324-print-words-vertically/1324-print-words-vertically.py
@@ -0,0 +1,24 @@
+class Solution:
+    def printVertically(self, s: str) -> List[str]:
+        # split s into a list
+        s = s.split()
+        
+        # find out the longest word and at which index in happens
+        max_len = max(map(len, s))
+
+        # create the object to be returned
+        vertical = defaultdict(list)
+        
+        # iterate through the string
+        for word_idx, word in enumerate(s):
+            for char_idx, char in enumerate(word):
+                while len(vertical[char_idx]) < word_idx:
+                    vertical[char_idx].append(" ")
+                vertical[char_idx].append(char)
+        
+        # collect the values
+        vertical_words = []
+        for value in vertical.values():
+            vertical_words.append("".join(value))
+
+        return vertical_words
