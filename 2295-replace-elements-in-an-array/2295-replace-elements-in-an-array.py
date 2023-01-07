0888ac90c9cddbfdfaa065e37cec1cc07afbbd21
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-01-07 09:22:44 +0300

    Time: 1462 ms (71.73%), Space: 70.1 MB (18.14%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.py
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.py
@@ -1,4 +1,11 @@
 class Solution:
+    def changeDictKey(self, dictionary: dict, new_key: any, old_key: any) -> None:
+        """Change a dictionary key to a new one while keeping the value.
+        It is assumed the dictionary will already contain the old_key and its value.
+        """
+        dictionary[new_key] = dictionary[old_key]
+        del dictionary[old_key]
+
     def arrayChange(self, nums: List[int], operations: List[List[int]]) -> List[int]:
         # create value to index dictionary
         indexes = dict(zip(nums, range(len(nums))))
@@ -6,7 +13,8 @@ class Solution:
         # iterate through the operations and perform the said operations
         for operation in operations:
             nums[indexes[operation[0]]] = operation[1]
-            indexes[operation[1]] = indexes[operation[0]]
-            del indexes[operation[0]]
+            
+            # change the indexes dictionary to incorporate the changes
+            self.changeDictKey(indexes, operation[1], operation[0])
         
         return nums
