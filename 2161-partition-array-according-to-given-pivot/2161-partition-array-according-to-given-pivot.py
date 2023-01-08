016b623fa6e4b15eccefeac0666f866e9e053524
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-01-08 21:42:24 +0300

    Time: 1544 ms (69.93%), Space: 31.4 MB (66.97%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -1,8 +1,20 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        less_than_pivot = [num for num in nums if num < pivot]
-        equal_to_pivot = [num for num in nums if num == pivot]
-        greater_than_pivot = [num for num in nums if num > pivot]
+        # create the object to be returned
+        partitioned = []
         
-        return less_than_pivot + equal_to_pivot + greater_than_pivot
+        # loop thorugh nums three times to add all elements to the list
+        for num in nums:
+            if num < pivot:
+                partitioned.append(num)
+                
+        for num in nums:
+            if num == pivot:
+                partitioned.append(num)
+                
+        for num in nums:
+            if num > pivot:
+                partitioned.append(num)
+        
+        return partitioned
         
