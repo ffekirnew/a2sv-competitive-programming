ba4540614e089ab457c7bacf77b1f15aa1186260
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-01-08 16:46:01 +0300

    Time: 37 ms (70.61%), Space: 13.9 MB (57.00%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
@@ -1,18 +1,25 @@
 
 class Solution:
     def maxDepth(self, s: str) -> int:
+        # time = O(n)
+        # space = 
+        
+        
+        
         # create the obj. to be returned
-        max_depth = 0
-        curr_depth = 0
+        max_depth = 0 # O(1)
+        curr_depth = 0 # O(1)
+        
+        opening, closing = '(', ')'
         
         # iterate through the string
-        for char in s:
-            if char == '(':
+        for char in s: # O(n) for n is the len(s)
+            if char == opening: # O(1)
                 curr_depth += 1
-            elif char == ')':
+            elif char == closing: # O(1)
                 curr_depth -= 1
             
-            max_depth = max(max_depth, curr_depth)
+            max_depth = max(max_depth, curr_depth) # O(1)
         
         # return the object
-        return max_depth
+        return max_depth # O(1)
