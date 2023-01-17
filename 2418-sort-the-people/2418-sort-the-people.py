adea38e28a13f6c171d3c3a53c4a08c3809b4038
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-01-17 18:57:00 +0300

    Time: 582 ms (10.67%), Space: 14.4 MB (84.60%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,14 +1,15 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        # do bubble sort
-        for i in range(len(heights)):
-            bubbled = False
-            for j in range(1, len(heights) - i):
-                if heights[j] > heights[j - 1]:
-                    bubbled = True
-                    heights[j], heights[j - 1] = heights[j - 1], heights[j]
-                    names[j], names[j - 1] = names[j - 1], names[j]
-            if not bubbled:
-                break
+        # do selection sort
+        for swap_idx in range(len(heights)):
+            min_idx = swap_idx
+
+            for idx in range(swap_idx, len(heights)):
+                if heights[idx] > heights[min_idx]:
+                    min_idx = idx
+            
+            heights[min_idx], heights[swap_idx] = heights[swap_idx], heights[min_idx]
+            names[min_idx], names[swap_idx] = names[swap_idx], names[min_idx]
+
         return names
         
