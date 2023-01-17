757d53d7fc713d631da9aed7f63b6d31f7753f2e
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-01-17 19:38:40 +0300

    Time: 1464 ms (5.49%), Space: 14.3 MB (84.60%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,15 +1,15 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         # do selection sort
-        for swap_idx in range(len(heights)):
-            min_idx = swap_idx
-
-            for idx in range(swap_idx, len(heights)):
-                if heights[idx] > heights[min_idx]:
-                    min_idx = idx
+        for idx in range(1, len(heights)):
+            curr_idx = idx
             
-            heights[min_idx], heights[swap_idx] = heights[swap_idx], heights[min_idx]
-            names[min_idx], names[swap_idx] = names[swap_idx], names[min_idx]
-
+            while curr_idx > 0:
+                if heights[curr_idx] > heights[curr_idx - 1]:
+                    heights[curr_idx], heights[curr_idx - 1] = heights[curr_idx - 1], heights[curr_idx]
+                    names[curr_idx], names[curr_idx - 1] = names[curr_idx - 1], names[curr_idx]
+                    curr_idx -= 1
+                else:
+                    break
         return names
         
