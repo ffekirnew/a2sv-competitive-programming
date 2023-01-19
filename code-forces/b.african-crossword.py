05fcd2f5fd5426e7eecf03699c7e53e8dc3cc8c2
ffekirnew <ffekirnew0808@gmail.com>
2023-01-19 05:38:54 +0300

    added the solution to B. African Crossword at CodeForces

--- code-forces/b.african-crossword.py
+++ code-forces/b.african-crossword.py
@@ -2,35 +2,26 @@ from collections import defaultdict
 
 
 if __name__ == "__main__":
+    # accept the input
     rows, cols = map(int, input().split())
 
     grid = []
     for row in range(rows):
         grid.append(list(input()))
     
-    cols_freq = []
-    rows_freq = []
-
+    # count all columns and rows
+    cols_freq = [defaultdict(int) for r in range(rows)]
+    rows_freq = [defaultdict(int) for c in range(cols)]
     for r in range(rows):
-        rows_freq.append(defaultdict(int))
         for c in range(cols):
-            rows_freq[-1][grid[r][c]] += 1
-
-    for c in range(cols):
-        cols_freq.append(defaultdict(int))
-        for r in range(rows):
-            cols_freq[-1][grid[r][c]] += 1
+            rows_freq[r][grid[r][c]] += 1
+            cols_freq[c][grid[r][c]] += 1
     
+    # construct the answer
     answer = []
     for row in range(rows):
         for col in range(cols):
-            repeated = False
-            if rows_freq[row][grid[row][col]] > 1:
-                repeated = True
-            if cols_freq[col][grid[row][col]] > 1:
-                repeated = True
-            
-            if not repeated:
+            if not rows_freq[row][grid[row][col]] > 1 and not cols_freq[col][grid[row][col]] > 1:
                 answer.append(grid[row][col])
     
     print("".join(answer))
