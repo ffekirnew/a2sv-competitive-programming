98bad93a73b70813fe64ced881f7b7bcb5eebb27
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-02-24 17:01:22 +0300

    Time: 343 ms (9.53%), Space: 18.9 MB (55.35%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -1,17 +1,19 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        # create the object to be returned
+        freq = defaultdict(int)
+        freq[0] = 1
+        
         answer = 0
-        # use a freq hashmap to keep track of previous remainders
-        prev = {0:1}
-        curr = 0
-        # loop through nums
-        for num in nums:
-            curr += num
-            div = curr % k
-            if (div) in prev:
-                answer += prev[div]
-            prev[div] = prev.get(div, 0) + 1
-        # return the solution
+        
+        for i, num in enumerate(nums):
+            nums[i] += nums[i - 1] if i > 0 else 0
+            nums[i] %= k
+            
+            if nums[i] in freq:
+                answer += freq[nums[i]]
+            
+            freq[nums[i]] += 1
+        
         return answer
+                
         
