b10adab091899055512ab04c85f11eddce34cac4
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-02-24 16:25:15 +0300

    Time: 1344 ms (82.26%), Space: 39.7 MB (14.52%) - LeetHub

---
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        zeros = [ 0 ] * ( len(s) + 1 )
+        
+        for shift in shifts:
+            zeros[ shift[0] ] += 1 if shift[2] else -1
+            zeros[ shift[1] + 1 ] += -1 if shift[2] else 1
+        
+        for i, zero in enumerate(zeros):
+            zeros[i] += zeros[i - 1] if i > 0 else 0
+        
+        print(zeros)
+        
+        answer = []
+        
+        for i, char in enumerate(s):
+            target = ord(char) + zeros[i] - 97
+            target %= 26
+            answer.append(chr( target + 97 ))
+        
+        return "".join(answer)
+            
+        
+        
