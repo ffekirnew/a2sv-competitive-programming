e8f2b7422ff1551d76f105e3df70430023126418
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-02-26 19:13:15 +0300

    Time: 454 ms (93.12%), Space: 18.9 MB (58.85%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -1,13 +1,14 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
 
-        arr,n = [0]+arr, len(arr)+1
-        ans, queue  = [0]*n, deque([0])
+        arr,n = [0] + arr, len(arr) + 1
+
+        ans, queue  = [0] * n, deque([0])
 
         for i in range(n):
             while arr[queue[0]] > arr[i]:
                 queue.popleft()
-            ans[i] = ans[queue[0]] + (i-queue[0])*arr[i]
+            ans[i] = ans[queue[0]] + (i-queue[0]) * arr[i]
             queue.appendleft(i)
 
-        return sum(ans) % 1000000007
+        return sum(ans) % (10 ** 9 + 7)
