c135da54bcf95d20fd108432e846ee8c979a71cc
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-03-01 15:19:18 +0300

    Time: 590 ms (27.21%), Space: 19.1 MB (19.45%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -1,14 +1,42 @@
 class Solution:
-    def sumSubarrayMins(self, arr: List[int]) -> int:
+    def nextSmaller(self, arr: List[int]) -> int:
+        n = len(arr)
+        next_smaller = [0] * len(arr)
 
-        arr,n = [0] + arr, len(arr) + 1
+        stack = []        
+        for i in range(n - 1, -1, -1):
+            while stack and stack[-1][0] >= arr[i]:
+                stack.pop()
+            
+            next_smaller[i] = stack[-1][1] if stack else n
+            stack.append( [ arr[i], i ] )
 
-        ans, queue  = [0] * n, deque([0])
-
+        return next_smaller
+    
+    def prevSmaller(self, arr: List[int]) -> int:
+        n = len(arr)
+        prev_smaller = [0] * len(arr)
+        
+        stack = []        
         for i in range(n):
-            while arr[queue[0]] > arr[i]:
-                queue.popleft()
-            ans[i] = ans[queue[0]] + (i-queue[0]) * arr[i]
-            queue.appendleft(i)
+            while stack and stack[-1][0] > arr[i]:
+                stack.pop()
+            
+            prev_smaller[i] = stack[-1][1] if stack else -1
+            stack.append( [ arr[i], i ] )
+        
+        return prev_smaller
 
-        return sum(ans) % (10 ** 9 + 7)
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        sum_of_subarray_mins = 0
+        
+        prev_smaller = self.prevSmaller(arr)
+        next_smaller = self.nextSmaller(arr)
+        
+        for i, num in enumerate(arr):
+            sum_of_subarray_mins += num * (i - prev_smaller[i]) * (next_smaller[i] - i)
+        
+        return sum_of_subarray_mins % (10 ** 9 + 7)
+        
+        
+            
