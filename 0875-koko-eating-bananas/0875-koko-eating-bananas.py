42104a82161c298b890447691d104c92dfec2037
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-03-03 17:56:28 +0300

    Time: 426 ms (88.36%), Space: 15.4 MB (64.11%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -9,7 +9,7 @@ class Solution:
             total_hours = sum([ math.ceil( pile / mid ) for pile in piles ])
             
             if total_hours == h:
-                prev_total_hours = sum([ math.ceil( pile / (mid - 1 )) for pile in piles ])
+                prev_total_hours = sum([ math.ceil( pile / (mid - 1 )) for pile in piles ]) if mid > 1 else inf
                 
                 if prev_total_hours != h:
                     return mid
