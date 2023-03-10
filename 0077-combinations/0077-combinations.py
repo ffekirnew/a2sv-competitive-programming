6a4d11f0cd867aa3a4f5c65ca3fb2dabde6f3bed
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-03-10 07:38:29 +0300

    Time: 464 ms (30.10%), Space: 15.8 MB (82.97%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,19 +1,23 @@
 class Solution:
-    def combine(self, n: int, k: int) -> List[List[int]]:
-        # do the solution
-        def backtrack(curr_combination=[], next_start=1):
-            if k - len(curr_combination) == 0:
-                answer.append(curr_combination[:])
-                return
-            
-            for candidate in range(next_start, n + 1):
-                curr_combination.append(candidate)
-                backtrack(curr_combination, candidate + 1)
-                curr_combination.pop()
+    def backtrack(self, nums, k_remains, comb, combs, i):
+        if not k_remains:
+            combs.append(comb.copy())
+            return
+        if i == len(nums):
+            return
+        
+        comb.append(nums[i])
+        self.backtrack(nums, k_remains - 1, comb, combs, i + 1)
+        comb.pop()
+        self.backtrack(nums, k_remains, comb, combs, i + 1)
+        
+        
 
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        nums = [num for num in range(1, n + 1)]
         # create the object to be returned
         answer = []
         # call the backtrack
-        backtrack()
+        self.backtrack(nums, k, [], answer, 0)
         # return the solution
         return answer
