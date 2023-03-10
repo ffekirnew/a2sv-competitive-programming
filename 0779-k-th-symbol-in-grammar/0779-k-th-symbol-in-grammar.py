ee90a5821b7a869b3794d13fc3e29a1be3b0d710
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-03-10 17:53:54 +0300

    Time: 32 ms (59.98%), Space: 13.9 MB (11.17%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -3,7 +3,7 @@ class Solution:
         # base case if n is 1 and k is 1
         if n == 1 and k == 1:
             return 0
-        mid = 2**(n-2)
+        mid = 2 ** (n - 2)
         if k <= mid:
             return self.kthGrammar(n - 1, k)
         else:
