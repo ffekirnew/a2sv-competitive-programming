8bf9cebfb980a1204e01899f5fad51886281b0e2
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-03-21 19:04:17 +0300

    Time: 31 ms (89.44%), Space: 14.2 MB (67.89%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,22 +1,18 @@
 class Solution:
     def backtrack(self, opening_left, closing_left, curr):
         if not opening_left and not closing_left:
-            self.parentheses.append("".join(curr))
+            self.parentheses.append(curr)
 
         if not curr or opening_left:
-            curr.append('(')
-            self.backtrack(opening_left - 1, closing_left, curr)
-            curr.pop()
+            self.backtrack(opening_left - 1, closing_left, curr + '(')
         if opening_left < closing_left:
-            curr.append(')')
-            self.backtrack(opening_left, closing_left - 1, curr)
-            curr.pop()
+            self.backtrack(opening_left, closing_left - 1, curr + ')')
             
     def generateParenthesis(self, n: int) -> List[str]:
         self.n = n
         self.parentheses = []
         
-        self.backtrack(n, n, [])
+        self.backtrack(n, n, "")
         
         return self.parentheses
         
