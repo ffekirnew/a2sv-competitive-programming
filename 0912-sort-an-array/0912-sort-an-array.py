cef03e31413425cf03132f3d00254a75077d6079
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-03-22 17:55:52 +0300

    Time: 2570 ms (14.81%), Space: 22.9 MB (36.25%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,25 +1,25 @@
 class Solution:
     def merge(self, left, right):
         i, j = 0, 0
-        sorted = []
+        sorted_array = []
         
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                sorted.append(left[i])
+        while i < len(left) or j < len(right):
+            if i == len(left):
+                sorted_array.append(right[j])
+                j += 1
+
+            elif j == len(right):
+                sorted_array.append(left[i])
+                i += 1
+
+            elif left[i] < right[j]:
+                sorted_array.append(left[i])
                 i += 1
             else:
-                sorted.append(right[j])
+                sorted_array.append(right[j])
                 j += 1
         
-        while i < len(left):
-            sorted.append(left[i])
-            i += 1
-        
-        while j < len(right):
-            sorted.append(right[j])
-            j += 1
-        
-        return sorted
+        return sorted_array
         
         
     def sortArray(self, nums: List[int]) -> List[int]:
