d90a52581bdafc6b5ef48af80bd88acf39ef0fc3
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-04-04 08:10:15 +0300

    Time: 109 ms (80.87%), Space: 14.6 MB (47.10%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -1,16 +1,15 @@
 class Solution:
     def partitionString(self, s: str) -> int:
-        answer = [[]]
+        answer = 1
         curr_set = set()
         
         for char in s:
             if char in curr_set:
                 curr_set.clear()
-                answer.append([])
+                answer += 1
 
             curr_set.add(char)
-            answer[-1].append(s)
         
-        return len(answer)
+        return answer
             
         
