25f658a3df08d7f6a19f924ccee1ad92b5c3bc91
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-04-11 18:44:20 +0300

    Time: 101 ms (68.67%), Space: 15.6 MB (96.64%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,26 +1,15 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        is_goal_node = lambda x: x == len(graph) - 1
-        
-        def dfs(start):
-            answer = []
-            fringe = []
-
-            fringe.append([start, [start]])
-
-            while fringe:
-                curr_node, curr_path = fringe.pop()
-
-                if is_goal_node(curr_node):
-                    answer.append(curr_path.copy())
-                    continue
-                    
-                for node in graph[curr_node]:
-                    fringe.append((node, curr_path + [node]))
+        paths = []
+        def dfs(curr, target):
+            if curr[-1] == target:
+                paths.append(curr)
+                return
             
-            return answer
-        
-        return dfs(0)
+            for nextval in graph[curr[-1]]:
+                dfs(curr + [nextval], target)
 
-            
-        
+        dfs([0], len(graph) - 1)
+
+        return paths
+
