a420f9288a6215f13ccef9c9f81398040b7c1b11
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-04-26 18:38:09 +0300

    Time: 75 ms (5.24%), Space: 16.3 MB (7.64%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -2,51 +2,41 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
         DIRS = [ [0, 1], [0, -1], [1, 0], [-1, 0] ]
-        
-        def in_bound(r, c):
-            return 0 <= r < ROWS and 0 <= c < COLS
+        IN_BOUND = lambda r, c: 0 <= r < ROWS and 0 <= c < COLS
         
         def bfs(rotten_list):
             if not rotten_list:
                 return 0
 
             visited = set()
-            fringe = rotten_list
             minutes = -1
             
-            while fringe:
+            while rotten_list:
                 minutes += 1
                 
                 next_level = []
                 
-                for row, col in fringe:
+                for row, col in rotten_list:
                     grid[row][col] = 2
                     for r, c in DIRS:
                         nx_row, nx_col = row + r, col + c
                         
-                        if (nx_row, nx_col) not in visited and in_bound(nx_row, nx_col) and grid[nx_row][nx_col] == 1:
+                        if (nx_row, nx_col) not in visited and IN_BOUND(nx_row, nx_col) and grid[nx_row][nx_col] == 1:
                             visited.add((nx_row, nx_col))
                             next_level.append((nx_row, nx_col,))
                 
-                fringe = next_level
+                rotten_list = next_level
                 
             return minutes
         
-        rotten_list = []
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == 2:
-                    rotten_list.append((r, c,))
+        rotten_list = [(r, c) for r in range(ROWS) for c in range(COLS) if grid[r][c] == 2]
                     
         answer = bfs(rotten_list)
         
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == 1:
-                    return -1
-        
-        
-        return answer
+        if all(grid[r][c] != 1 for r in range(ROWS) for c in range(COLS)):
+            return answer
+
+        return -1
                     
                     
                     
