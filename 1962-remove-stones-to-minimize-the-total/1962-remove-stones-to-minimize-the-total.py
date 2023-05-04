cc27e337a80538e2b9c9dcd676b7471feb14fcd1
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-05-04 19:32:36 +0300

    Time: 1856 ms (43.57%), Space: 31.3 MB (6.44%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.py
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.py
@@ -1,9 +1,10 @@
 class Solution:
     def minStoneSum(self, piles: List[int], k: int) -> int:
+        # have the piles in a max-heap
         piles = list(map(lambda x: -x, piles))
-        
         heapify(piles)
         
+        # for the amount of k, execute the operation
         while k:
             number = heappop(piles)
             number += -1 * number // 2
@@ -12,8 +13,10 @@ class Solution:
             k -= 1
         
         
+        # make the elements in the piles positive again
         piles = list(map(lambda x: -x, piles))
         
+        # return the sum
         return sum(piles)
         
         
