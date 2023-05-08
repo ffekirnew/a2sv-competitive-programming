c1d9bc244529437e90d2a8fe3b38f6ba54c2f75e
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-05-08 18:26:33 +0300

    Time: 114 ms (23.88%), Space: 18 MB (14.18%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -6,28 +6,20 @@ class Solution:
         for course, pre_requisite in prerequisites:
             in_degree[course] += 1
             graph[pre_requisite].append(course)
-            
         
         queue = deque([course for course in graph.keys() if in_degree[course] == 0])
-        visited = set()
-        path = []
+        order = []
         
         while queue:
             curr_course = queue.popleft()
             
-            path.append(curr_course)
-            visited.add(curr_course)
+            order.append(curr_course)
             
             for new_course in graph[curr_course]:
-                if new_course in visited:
-                    return []
-
                 in_degree[new_course] -= 1
                 
                 if in_degree[new_course] == 0:
                     queue.append(new_course)
 
-        if len(path) != numCourses:
-            return []
-        return path
+        return order if len(order) == numCourses else []
             
