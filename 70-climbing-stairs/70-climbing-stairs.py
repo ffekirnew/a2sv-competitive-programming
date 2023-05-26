28b5ba4ef56b8a6fa498cb6f955738a63a212ee3
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-05-26 18:05:14 +0300

    Time: 39 ms (47.04%), Space: 16.5 MB (8.13%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,5 +1,4 @@
 class Solution:
     @cache
-    def climbStairs(self, n: int) -> int:
-        return 0 if n < 0 else 1 if n == 0 else self.climbStairs(n - 1) + self.climbStairs(n - 2)
+    def climbStairs(self, n: int) -> int: return 0 if n < 0 else 1 if n == 0 else self.climbStairs(n - 1) + self.climbStairs(n - 2)
         
