8d0d8dad6fe7af2b8dc8e269dcfdb06173c46c5a
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-05-31 19:01:14 +0300

    Time: 57 ms (38.13%), Space: 16.5 MB (13.11%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -1,22 +1,20 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         ROWS, COLS = len(obstacleGrid), len(obstacleGrid[0])
-        DIRS = [ [0, 1], [1, 0] ]
-        
-        paths = [[0 for col in range(COLS)] for row in range(ROWS)]
-        paths[-1][-1] = 1
+        obstacleGrid[-1][-1] = 1 - obstacleGrid[-1][-1]
         
         for row in range(ROWS - 1, -1, -1):
             for col in range(COLS - 1, -1, -1):
+                if row == ROWS - 1 and col == COLS - 1:
+                    continue
                 if obstacleGrid[row][col]:
-                    paths[row][col] = 0
-                
+                    obstacleGrid[row][col] = 0
                 else:
                     if row + 1 < ROWS:
-                        paths[row][col] += paths[row + 1][col]
+                        obstacleGrid[row][col] += obstacleGrid[row + 1][col]
                     if col + 1 < COLS:
-                        paths[row][col] += paths[row][col + 1]
+                        obstacleGrid[row][col] += obstacleGrid[row][col + 1]
         
-        return paths[0][0]
+        return obstacleGrid[0][0]
             
         
