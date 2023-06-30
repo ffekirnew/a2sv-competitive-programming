c09b7ac05408675148ae6500e37905705fd3536f
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-06-30 08:48:56 +0300

    Time: 105 ms (73.65%), Space: 16.5 MB (33.53%) - LeetHub

--- 1377-frog-position-after-t-seconds/1377-frog-position-after-t-seconds.py
+++ 1377-frog-position-after-t-seconds/1377-frog-position-after-t-seconds.py
@@ -7,8 +7,11 @@ class Solution:
         for node1, node2 in edges:
             graph[node1].add(node2)
             graph[node2].add(node1)
+            
+        stack = [(1, 1, 0)]
+        while stack:
+            node, probability, time = stack.pop()
 
-        def dfs(node, probability, time):
             if node == target and time == t:
                 return probability
             
@@ -16,12 +19,9 @@ class Solution:
             if node == target and t > time and not children:
                 return probability
 
-            target_probability = 0
             for child in children:
                 graph[child].remove(node)
-                target_probability = max(dfs(child, probability / len(children), time + 1), target_probability)
-            
-            return target_probability
+                stack.append((child, probability / len(children), time + 1))
         
-        return dfs(1, 1, 0)
+        return 0
         
