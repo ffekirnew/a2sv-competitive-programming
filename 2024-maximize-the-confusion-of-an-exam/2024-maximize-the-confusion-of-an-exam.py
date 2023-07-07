38f5fc220675cebbbfa9cd1a39c50939e7b80d76
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-07-07 14:33:40 +0300

    Time: 471 ms (39.02%), Space: 16.8 MB (55.49%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
@@ -1,22 +1,20 @@
 class Solution:
-    @staticmethod
-    def maxConsecutiveWithChange(answers, wanted_value, k):
-        max_length = 0
-
-        changed = 0
-        left = 0
-        for right in range(len(answers)):
-            changed += answers[right] != wanted_value
-            
-            while changed > k:
-                changed -= answers[left] != wanted_value
-                left += 1
-            
-            max_length = max(max_length, right - left + 1)
-        
-        return max_length
-            
-                
     def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
-        return max( self.maxConsecutiveWithChange(answerKey, 'T', k), self.maxConsecutiveWithChange(answerKey, 'F', k) )
+        # create the object to be returned
+        answer = 0
+
+        # do it to make max consec T's ones and then F's
+        for current_protagonist in ['T', 'F']:
+            # set up the sliding window
+            s, ops = 0, 0
+            for e in range(len(answerKey)):
+                ops += answerKey[e] != current_protagonist # operation needed
+                while ops > k:
+                    ops -= answerKey[s] != current_protagonist # operation cleared
+                    s += 1
+
+                answer = max(answer, e - s + 1)
+
+        # return the solution
+        return answer
         
