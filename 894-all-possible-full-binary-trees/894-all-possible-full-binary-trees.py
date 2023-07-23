affe011a812fc9edecbf64a3d263db46c1e5806d
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-07-23 13:55:49 +0300

    Time: 179 ms (76.85%), Space: 19.9 MB (80.50%) - LeetHub

---
+++ 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
+        if n % 2 == 0:
+            return []
+
+        possible_FBTs = []
+        memo = {1: [TreeNode()]}
+        
+        def dp(n):
+            if n not in memo:
+                memo[n] = []
+                
+                for left_nodes in range(1, n, 1):
+                    for possible_left in dp(left_nodes):
+                        for possible_right in dp(n - left_nodes - 1):
+                            node = TreeNode(0, possible_left, possible_right)
+                            memo[n].append(node)                            
+            
+            return memo[n]
+        
+        return dp(n)
+                        
+        
+        
+        
