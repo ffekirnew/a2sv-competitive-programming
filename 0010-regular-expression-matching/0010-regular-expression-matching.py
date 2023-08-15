446e125780ae6ca9351c7a134927bd2cc8293b7b
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-08-15 11:59:04 +0300

    Time: 41 ms (96.63%), Space: 16.6 MB (39.05%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,40 +1,20 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        def is_alpha_or_dot(char):
-            return char == '.' or 'a' <= char <= 'z'
-
-        @cache
+        @functools.lru_cache(None)  # Use functools.lru_cache for memoization
         def match(i1, i2):
-            # Base Cases
-            if i1 >= len(s) and i2 >= len(p):
-                return True
-
-            if i2 + 1 < len(p) and is_alpha_or_dot(p[i2]) and p[i2 + 1] == '*':
-                is_match = False
-                breaked = False
-                i = i1
-                for i in range(i1, len(s)):
-                    if is_match:
-                        return is_match
-
-                    if p[i2].isalpha() and p[i2] != s[i]:
-                        breaked = True
-                        break
-
-                    is_match |= match(i, i2 + 2)
-                    
-                return is_match or (match(i, i2 + 2) if breaked else match(i + 1, i2 + 2))
-
-            if i1 >= len(s) or i2 >= len(p):
-                return False
-
-            if is_alpha_or_dot(p[i2]) and (i2 + 1 >= len(p) or p[i2 + 1] != '*'):
-                return (p[i2] == '.' or s[i1] == p[i2]) and match(i1 + 1, i2 + 1)
+            if i2 >= len(p):
+                return i1 >= len(s)
             
-            return True
+            first_match = i1 < len(s) and (s[i1] == p[i2] or p[i2] == '.')
+            
+            if i2 + 1 < len(p) and p[i2 + 1] == '*':
+                return (match(i1, i2 + 2) or 
+                        (first_match and match(i1 + 1, i2)))
+            
+            return first_match and match(i1 + 1, i2 + 1)
         
-
         return match(0, 0)
+
                 
                     
             
