2ea55e595a2faeea0d9b6a0534c00f843b085ebc
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-09-25 16:40:24 +0300

    Time: 6388 ms (5.47%), Space: 111.9 MB (5.09%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -1,13 +1,10 @@
 class Solution:
     def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
         scores_and_ages = [(ages[i], scores[i]) for i in range(len(scores))]
-        del scores
-        del ages
-        
         scores_and_ages.sort()
         
         @cache
-        def dp(index: int, highest_so_far: int) -> int:
+        def dp(index: int, highest_so_far: Tuple[int, int]) -> int:
             if index == len(scores_and_ages):
                 return 0
             
@@ -22,8 +19,6 @@ class Solution:
                 
                 if score >= highest_score:
                     highest_possible = max(highest_possible, score + dp(index + 1, (age, score)))
-                else:
-                    highest_possible = max(highest_possible, score + dp(index + 1, (highest_score_age, highest_score)))
                 
                 return highest_possible
            
