b65ad95d60b56bbdb871bc5997d1f9288e9eeffb
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-09-26 16:38:28 +0300

    Time: 855 ms (33.12%), Space: 76.2 MB (5.09%) - LeetHub

--- 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.py
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.py
@@ -1,14 +1,15 @@
 class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
-        trie = [None for __ in range(len(s) + 1)]
-        for i in range(len(s) - 1, -2, -1):
-            if i == len(s) - 1:
-                trie[i + 1] = {}
-            else:
-                new_trie_node = trie[i + 2].copy()
-                new_trie_node[s[i + 1]] = i + 1
+        trie = [None for _ in range(len(s) + 1)]
 
-                trie[i + 1] = new_trie_node
+        for index in range(len(s) - 1, -2, -1):
+            if index == len(s) - 1:
+                trie[index + 1] = {}
+            else:
+                new_trie_node = trie[index + 2].copy()
+                new_trie_node[s[index + 1]] = index + 1
+
+                trie[index + 1] = new_trie_node
 
         counter = 0
         for word in words:
