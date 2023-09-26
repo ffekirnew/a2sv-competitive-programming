56b5f3c715e3d6e3128703c528010f94439abe3f
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-09-26 15:08:16 +0300

    Time: 38 ms (75.72%), Space: 16.4 MB (68.63%) - LeetHub

--- 0677-map-sum-pairs/0677-map-sum-pairs.py
+++ 0677-map-sum-pairs/0677-map-sum-pairs.py
@@ -2,9 +2,6 @@ class TrieNode:
     def __init__(self, value: int = 0):
         self.children = {}
         self.value = value
-    
-    def __str__(self):
-        return f"Value: {self.value}, Children: {self.children}"
 
 
 class Trie:
@@ -13,24 +10,17 @@ class Trie:
 
     def insert(self, key: str, value: int) -> None:
         curr = self.root
-
         for i, char in enumerate(key):
-            if char not in curr.children:
-                curr.children[char] = TrieNode(value)
-            else:
-                curr.children[char].value += value
-
+            if char not in curr.children: curr.children[char] = TrieNode(value)
+            else: curr.children[char].value += value
             curr = curr.children[char]
 
     def search(self, word: str) -> bool:
         curr = self.root
-
         for i, char in enumerate(word):
             if char not in curr.children:
                 return 0
-
             curr = curr.children[char]
-
         return curr.value
 
 class MapSum:
@@ -41,9 +31,7 @@ class MapSum:
 
     def insert(self, key: str, val: int) -> None:
         if key in self.words:
-            temp = val
-            val -= self.words[key]
-            self.words[key] = temp
+            self.words[key], val = val, val - self.words[key]
         else:
             self.words[key] = val
 
