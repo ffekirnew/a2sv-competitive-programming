743ac8e287e5b097d7915c8c46551b9dccccd791
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-09-29 14:47:45 +0300

    Time: 39 ms (48.25%), Space: 16.5 MB (14.48%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -0,0 +1,77 @@
+from typing import List
+
+class TrieNode:
+    def __init__(self, is_end_of_word: int = 0, word_index: int = -1):
+        self.children = {}
+        self.is_end_of_word = is_end_of_word
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+
+        for i, char in enumerate(word):
+            char_is_end_of_word = i == len(word) - 1
+            if char not in curr.children:
+                curr.children[char] = TrieNode(char_is_end_of_word)
+            else:
+                curr.children[char].is_end_of_word |= char_is_end_of_word
+
+            curr = curr.children[char]
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+
+        for i, char in enumerate(word):
+            if char not in curr.children:
+                return False
+
+            curr = curr.children[char]
+
+        return curr.is_end_of_word
+
+    def starts_with(self, prefix: str) -> bool:
+        curr = self.root
+
+        for char in prefix:
+            if char not in curr.children:
+                return False
+            curr = curr.children[char]
+
+        return True
+
+    @classmethod
+    def build_trie(cls, words: List[str]) -> "Trie":
+        trie = cls()
+
+        for word in words:
+            trie.insert(word)
+
+        return trie
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        dictionary = Trie.build_trie(wordDict)
+        
+        @cache
+        def dp(word_start: int, curr_index: int) -> List[List[str]]:
+            if curr_index == len(s):
+                return []
+            
+            possible_sentences = []
+            curr_word = s[word_start:curr_index+1]
+            if dictionary.search(curr_word):
+                following_sentences = dp(curr_index + 1, curr_index + 1)
+                
+                if following_sentences:
+                    possible_sentences = [ [curr_word] + sentence for sentence in following_sentences ]
+                elif curr_index + 1 == len(s):
+                    possible_sentences = [ [curr_word] ]
+            
+            possible_sentences += dp(word_start, curr_index + 1)
+            
+            return possible_sentences
+        
+        return [" ".join(sentence) for sentence in dp(0, 0)]
