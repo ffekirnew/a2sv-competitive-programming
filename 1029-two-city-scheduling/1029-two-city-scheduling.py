8e1f7b6995b6a22a2565f8b43600d75cdca42852
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-10-02 16:28:16 +0300

    Time: 72 ms (7.47%), Space: 17.8 MB (6.54%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -8,12 +8,12 @@ class Solution:
             if index == len(costs):
                 return 0
 
-            send_to_city_a, send_to_city_b = inf, inf
+            cost_send_to_city_a, cost_send_to_city_b = inf, inf
             if city_a_left:
-                send_to_city_a = costs[index][0] + dp(index + 1, city_a_left - 1, city_b_left)
+                cost_send_to_city_a = costs[index][0] + dp(index + 1, city_a_left - 1, city_b_left)
             if city_b_left:
-                send_to_city_b = costs[index][1] + dp(index + 1, city_a_left, city_b_left - 1)
+                cost_send_to_city_b = costs[index][1] + dp(index + 1, city_a_left, city_b_left - 1)
             
-            return min(send_to_city_a, send_to_city_b)
+            return min(cost_send_to_city_a, cost_send_to_city_b)
         
         return dp(0, len(costs) // 2, len(costs) // 2)
