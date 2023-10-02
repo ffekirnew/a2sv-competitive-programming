c08bf41eb3cfc146d9205c06bd4cac16b734eb04
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-10-02 16:17:45 +0300

    Time: 73 ms (7.24%), Space: 17.7 MB (6.66%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -0,0 +1,19 @@
+from math import inf
+
+
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        @cache
+        def dp(index: int, city_a_left: int, city_b_left: int) -> int:
+            if index == len(costs):
+                return 0
+
+            send_to_city_a, send_to_city_b = inf, inf
+            if city_a_left:
+                send_to_city_a = costs[index][0] + dp(index + 1, city_a_left - 1, city_b_left)
+            if city_b_left:
+                send_to_city_b = costs[index][1] + dp(index + 1, city_a_left, city_b_left - 1)
+            
+            return min(send_to_city_a, send_to_city_b)
+        
+        return dp(0, len(costs) // 2, len(costs) // 2)
