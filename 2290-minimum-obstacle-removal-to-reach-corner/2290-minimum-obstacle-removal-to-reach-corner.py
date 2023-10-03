1b017555f04058ab70c1f49d2c151d25a1e893ce
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-10-03 11:43:27 +0300

    Time: 3677 ms (28.99%), Space: 58.8 MB (7.14%) - LeetHub

---
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
@@ -0,0 +1,31 @@
+from math import inf
+from heapq import heappop, heappush
+
+
+class Solution:
+    def minimumObstacles(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        distances = [[inf for _ in range(COLS)] for __ in range(ROWS)]
+        distances[0][0] = 0
+
+        dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        def is_in_bound(row, col):
+            return 0 <= row < ROWS and 0 <= col < COLS
+        
+        heap = [(0, 0, 0)] # [(weight, row, col),...]
+        visited = set([])
+        while heap:
+            weight, row, col = heappop(heap)
+            visited.add((row, col))
+            
+            if (row, col) == (ROWS - 1, COLS - 1):
+                return weight
+            
+            for r, c in dirs:
+                nx_row, nx_col = row + r, col + c
+                
+                if is_in_bound(nx_row, nx_col) and (nx_row, nx_col) not in visited and distances[nx_row][nx_col] > weight:
+                    distances[nx_row][nx_col] = weight
+                    heappush(heap, ( weight + grid[nx_row][nx_col], nx_row, nx_col ))
+        
+        return -inf
