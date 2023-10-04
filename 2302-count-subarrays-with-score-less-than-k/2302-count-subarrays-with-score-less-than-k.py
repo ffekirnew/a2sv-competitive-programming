d6b2f5f56d8d88177607e6e4d3d99f64f824eb2c
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-10-04 11:50:15 +0300

    Time: 1013 ms (22.37%), Space: 30.5 MB (87.50%) - LeetHub

---
+++ 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.py
@@ -0,0 +1,33 @@
+"""
+Solution Steps:
+1. Initialize return variable and sliding window
+2. Loop over the list using the right edge and move it one at a time
+    2.1. If the current number pointed at by right can be added to the running subarray, add it
+    2.2. If not, try moving the left pointer
+"""
+
+
+class CountSubarraysWithScoreLessThanK:
+    def sliding_window_solution(self, nums: List[int], k: int) -> int:
+        count = 0
+        
+        # do something
+        running_sum = 0
+        left = 0
+        for right, number in enumerate(nums):
+            while (number + running_sum)* (right - left + 1) >= k:
+                running_sum -= nums[left]
+                left += 1
+
+            if (number + running_sum) * (right - left + 1) < k:
+                count += (right - left + 1)
+                running_sum += number
+        
+        return count
+
+    
+class Solution:
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+        solution = CountSubarraysWithScoreLessThanK()
+        return solution.sliding_window_solution(nums, k)
+        
