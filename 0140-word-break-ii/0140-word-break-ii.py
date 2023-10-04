27ea185a2b5f0d46cb0eac4b26c620da7d7e1ec0
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-10-04 09:23:15 +0300

    Time: 27 ms (98.16%), Space: 16.4 MB (12.86%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -1,55 +1,5 @@
 from typing import List
 
-class TrieNode:
-    def __init__(self, is_end_of_word: int = 0, word_index: int = -1):
-        self.children = {}
-        self.is_end_of_word = is_end_of_word
-
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-
-    def insert(self, word: str) -> None:
-        curr = self.root
-
-        for i, char in enumerate(word):
-            char_is_end_of_word = i == len(word) - 1
-            if char not in curr.children:
-                curr.children[char] = TrieNode(char_is_end_of_word)
-            else:
-                curr.children[char].is_end_of_word |= char_is_end_of_word
-
-            curr = curr.children[char]
-
-    def search(self, word: str) -> bool:
-        curr = self.root
-
-        for i, char in enumerate(word):
-            if char not in curr.children:
-                return False
-
-            curr = curr.children[char]
-
-        return curr.is_end_of_word
-
-    def starts_with(self, prefix: str) -> bool:
-        curr = self.root
-
-        for char in prefix:
-            if char not in curr.children:
-                return False
-            curr = curr.children[char]
-
-        return True
-
-    @classmethod
-    def build_trie(cls, words: List[str]) -> "Trie":
-        trie = cls()
-
-        for word in words:
-            trie.insert(word)
-
-        return trie
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
@@ -64,12 +14,13 @@ class Solution:
 
             for index in range(curr_index, len(s)):
                 curr_word = s[curr_index : index + 1]
-                
+
                 if curr_word in dictionary:
                     following_sentences = dp(index + 1)
 
                     if following_sentences:
                         possible_sentences += [ [curr_word] + sentence for sentence in following_sentences ]
+
                     elif index + 1 == len(s):
                         possible_sentences += [ [curr_word] ]
             
