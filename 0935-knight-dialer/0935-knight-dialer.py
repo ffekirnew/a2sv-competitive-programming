f583cbd2e54ec59eb74ef0ee1f595b04544ff8b8
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-10-06 10:00:58 +0300

    Time: 1830 ms (30.71%), Space: 16.3 MB (84.38%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.py
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -5,23 +5,33 @@ Solution Steps:
 
 
 class KnightDialer:
-    def solution(self, length: int) -> int:
-        next_numbers = { 1: [6, 8], 2: [7, 9], 3: [4, 8], 4: [9, 3, 0], 5: [], 6: [1, 7, 0], 7: [2, 6], 8: [1, 3], 9: [4, 2], 0: [4, 6] }
+    def __init__(self):
+        self.next_numbers = { 1: [6, 8], 2: [7, 9], 3: [4, 8], 4: [9, 3, 0], 5: [], 6: [1, 7, 0], 7: [2, 6], 8: [1, 3], 9: [4, 2], 0: [4, 6] }
+        
+    def bottom_up(self, length: int) -> int:
         dp = [{1: 1} for _ in range(10)]
         
         for length in range(2, length + 1):
             for number in range(10):
-                dp[number][length] = sum(dp[next_number][length - 1] for next_number in next_numbers[number])
+                dp[number][length] = sum(dp[next_number][length - 1] for next_number in self.next_numbers[number])
             for number in range(10):
                 del dp[number][length - 1]
+
+        return sum(dp[number][length] for number in range(10))  
+
+    def top_down(self, length: int) -> int:
+        @cache
+        def dp(curr_number: int, length: int) -> int:
+            if length == 1:
+                return 1
             
-            
-        # print(dp)
-        return sum(dp[number][length] for number in range(10))        
+            return sum(dp(next_number, length - 1) for next_number in self.next_numbers[curr_number])
+        
+        return sum(dp(start, length) for start in range(10))   
 
 
 class Solution:
     def knightDialer(self, n: int) -> int:
         solution = KnightDialer()
-        return solution.solution(n) % (10 ** 9 + 7)
+        return solution.bottom_up(n) % (10 ** 9 + 7)
         
