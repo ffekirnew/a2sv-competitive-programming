c8a1b362d7ea5cae538019ffa27c09169ba137be
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-10-06 09:59:00 +0300

    Time: 1692 ms (33.65%), Space: 16.5 MB (63.68%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.py
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -7,14 +7,17 @@ Solution Steps:
 class KnightDialer:
     def solution(self, length: int) -> int:
         next_numbers = { 1: [6, 8], 2: [7, 9], 3: [4, 8], 4: [9, 3, 0], 5: [], 6: [1, 7, 0], 7: [2, 6], 8: [1, 3], 9: [4, 2], 0: [4, 6] }
-        @cache
-        def dp(curr_number: int, length: int) -> int:
-            if length == 1:
-                return 1
-            
-            return sum(dp(next_number, length - 1) for next_number in next_numbers[curr_number])
+        dp = [{1: 1} for _ in range(10)]
         
-        return sum(dp(start, length) for start in range(10))            
+        for length in range(2, length + 1):
+            for number in range(10):
+                dp[number][length] = sum(dp[next_number][length - 1] for next_number in next_numbers[number])
+            for number in range(10):
+                del dp[number][length - 1]
+            
+            
+        # print(dp)
+        return sum(dp[number][length] for number in range(10))        
 
 
 class Solution:
