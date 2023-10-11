83efd8a5eb0e1f9a4e2a6521d2308101c2ea419c
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-10-11 11:25:19 +0300

    Time: 914 ms (34.62%), Space: 16.3 MB (95.93%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.py
+++ 1395-count-number-of-teams/1395-count-number-of-teams.py
@@ -1,3 +1,18 @@
+"""
+Solution Steps:
+1. Iterate over each rating
+    1.1. For every rating, iterate over other ratings before it, since we want subsequences
+        1.1.1. If original rating is greater, update ending at that index and count the teams that can be formed based on the other index
+        1.1.2. If less, do the same but for decreasing ending at that index and count it
+2. Return the count
+
+Complexity Analysis:
+- Time Complexity: O(n ^ 2)
+- Space Complexity: O(n)
+    where: n is the length of the ratings list
+"""
+
+
 class CountNumberOfTeams:
     def __init__(self, rating: List[int]):
         self.rating = rating
@@ -13,7 +28,7 @@ class CountNumberOfTeams:
                 if self.rating[index] > self.rating[other_number_index]:
                     increasing_ending_at[index] += 1
                     teams_count += increasing_ending_at[other_number_index]
-                if self.rating[index] < self.rating[other_number_index]:
+                elif self.rating[index] < self.rating[other_number_index]:
                     decreasing_ending_at[index] += 1
                     teams_count += decreasing_ending_at[other_number_index]
                     
