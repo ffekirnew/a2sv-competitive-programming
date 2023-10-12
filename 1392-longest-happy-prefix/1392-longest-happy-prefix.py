f8070e35a3db050ad802195debed466639e2d6f1
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-10-12 14:32:11 +0300

    Time: 300 ms (57.89%), Space: 20.9 MB (46.32%) - LeetHub

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.py
@@ -0,0 +1,32 @@
+class LongestHappyPrefix:
+    def __init__(self, string: str):
+        self.string = string
+
+        self.lps = [0] * len(self.string)
+
+    def kmp(self):
+        prev_lps, index = 0, 1
+        while index < len(self.string):
+            if self.string[prev_lps] == self.string[index]:
+                self.lps[index] = prev_lps + 1
+                prev_lps += 1
+                index += 1
+                
+            else:
+                if prev_lps == 0:
+                    index += 1
+                else:
+                    prev_lps = self.lps[prev_lps - 1]
+        
+        # print(self.lps)
+        length = len(self.string)
+        return self.string[length - self.lps[-1]: length + 1]
+
+
+
+class Solution:
+    def longestPrefix(self, s: str) -> str:
+        solution = LongestHappyPrefix(s)
+        return solution.kmp()
+        
+        
