33749d0ec61b11a5b7c2512b638b8339d3ae2594
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-10-19 10:42:50 +0300

    Time: 790 ms (98.96%), Space: 27.6 MB (22.18%) - LeetHub

--- 2871-split-array-into-maximum-number-of-subarrays/2871-split-array-into-maximum-number-of-subarrays.py
+++ 2871-split-array-into-maximum-number-of-subarrays/2871-split-array-into-maximum-number-of-subarrays.py
@@ -1,21 +1,25 @@
-class Solution:
-    def maxSubarrays(self, nums: List[int]) -> int:
-        minimum_and = reduce(lambda x, y: x & y, nums)
+class SplitArrayIntoMaximumNumberOfSubarrays:
+    def __init__(self, nums: List[int]):
+        self.nums = nums
         
-        if minimum_and:
-            return 1
-        
+    def solve(self):
         counter = 0
         running_and = ~0
         
-        for num in nums:
+        for num in self.nums:
             running_and &= num
             
             if running_and == 0:
                 counter += 1
                 running_and = ~0
         
-        return counter
+        return max(counter, 1)
+
+
+class Solution:
+    def maxSubarrays(self, nums: List[int]) -> int:
+        solution = SplitArrayIntoMaximumNumberOfSubarrays(nums)
+        return solution.solve()
         
         
         
