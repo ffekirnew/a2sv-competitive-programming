0d0854d91820b4e6feb65d7b5862d9bef40125b8
Fikernew Birhanu <ffekirnew0808@gmail.com>
2023-11-07 12:49:36 +0300

    Time: 629 ms (15.21%), Space: 30.2 MB (36.01%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
@@ -1,28 +1,54 @@
-class Solution:
-    def getWinner(self, arr: List[int], k: int) -> int:
-        if k >= len(arr):
-            return max(arr)
-        
-        player1, player2 = 0, 1
-        win_streak = 0
-        winner = None
-        
-        while win_streak < k:
-            round_winner = arr[player1] if arr[player1] > arr[player2] else arr[player2]
-            
-            if arr[player1] > arr[player2]:
-                arr[player1], arr[player2] = arr[player2], arr[player1]
+"""
+Notes:
+- game between elements 0 and 1
 
-            if winner != round_winner:
-                winner = round_winner
-                win_streak = 0
-            
-            win_streak += 1
-            
-            player1 = (player1 + 1) % len(arr)
-            player2 = (player2 + 1) % len(arr)
+Solution Steps:
+1. Initialize a winner variable
+2. Initialize palyer1 and player2, and win_streak variable
+3. Loop throught the array
+    3.1. Check the winner, if winner is new, win_streak becomes 1
+    3.2. if winner is old, update win_streak by 1
+    3.3. if win_streak is k, break
+n. Return winner
+"""
+class FindTheWinnerOfAnArrayGame:
+    def __init__(self, arr, k):
+        self.arr = arr
+        self.k = k
+    
+    def approach_1(self):
+        if self.k > len(self.arr):
+            return max(self.arr)
+
+        winner = None
+        player_1_index, player_2_index = 0, 1
+        win_streak = 0
+        
+        while win_streak < self.k:
+            if self.arr[player_1_index] < self.arr[player_2_index]:
+                if winner == self.arr[player_2_index]:
+                    win_streak += 1
+                else:
+                    win_streak = 1
+                    winner = self.arr[player_2_index]
+
+            else:
+                if winner == self.arr[player_1_index]:
+                    win_streak += 1
+                else:
+                    win_streak = 1
+                    winner = self.arr[player_1_index]
+
+                self.arr[player_1_index], self.arr[player_2_index] = self.arr[player_2_index], self.arr[player_1_index]
+
+            player_1_index = (player_1_index + 1) % len(self.arr)
+            player_2_index = (player_2_index + 1) % len(self.arr)
         
         return winner
-        
-        
+
+
+class Solution:
+    def getWinner(self, arr: List[int], k: int) -> int:
+        solution = FindTheWinnerOfAnArrayGame(arr, k)
+        return solution.approach_1()
         
